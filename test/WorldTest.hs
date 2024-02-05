056bf6e659b8e1db0f8053c50e274756cd1f0af4
James Swent <jake@osprey21.com>
2024-02-05 11:18:01 +0000

    feat(tests): add property-based testing for World
    Defined arbitrary instances for GameData, ObjectType, WorldObject, Direction, RoomID, Exit, and Room. Created tests for won and notWon.
    
    NOTE: These tests must be refactored should `State` be implemented in place of GameData tuple

--- test/WorldTest.hs
+++ test/WorldTest.hs
@@ -17,17 +17,56 @@ runWorldTest = mapM_ (\(name, prop) -> do
 
 -- TODO: These tests will have to be refactored for implementing State
 
--- Define specific GameData instances for each test
-gameDataWon :: GameData
-gameDataWon = GameData { location_id = Street, inventory = [laptop]}
+-- Define arbitrary GameData
+instance Arbitrary GameData where
+    arbitrary = do
+        location <- arbitrary
+        world <- listOf ((,) <$> arbitrary <*> arbitrary)
+        items <- listOf arbitrary
+        poured <- arbitrary
+        caffeinated <- arbitrary
+        finished <- arbitrary
+        light <- arbitrary
+        return $ GameData location world items poured caffeinated finished light
 
-gameDataNotWon :: GameData
-gameDataNotWon = GameData { location_id = Bedroom, inventory = []}
+instance Arbitrary ObjectType where
+    arbitrary = elements [Mug, CoffeePot, Laptop]
+
+instance Arbitrary WorldObject where
+    arbitrary = do
+        objType <- arbitrary
+        return $ case objType of
+            Mug -> mug
+            CoffeePot -> coffeepot
+            Laptop -> laptop
+
+instance Arbitrary Direction where
+    arbitrary = elements [North, East, South, West, Out, In]
+
+instance Arbitrary RoomID where
+    arbitrary = elements [Bedroom, Kitchen, Hall, Street, Lounge]
+
+instance Arbitrary Exit where
+    arbitrary = do
+        direction <- arbitrary
+        description <- arbitrary
+        roomId <- arbitrary
+        return $ Exit direction description roomId
+
+instance Arbitrary Room where
+    arbitrary = do
+        roomId <- arbitrary
+        description <- arbitrary
+        exits <- listOf arbitrary
+        objects <- listOf arbitrary
+        return $ Room roomId description exits objects
 
 -- Property: If the player is in the street and has a laptop, they should have won
 prop_won :: Property
-prop_won = property $ won gameDataWon
+prop_won = forAll arbitrary $ \game_data ->
+    (location_id game_data == Street && (laptop `elem` inventory game_data)) === won game_data
 
 -- Property: If the player is not in the Street, they should not have won, regardless of their inventory
 prop_notWon :: Property
-prop_notWon = property $ not (won gameDataNotWon)
+prop_notWon = forAll arbitrary $ \game_data -> 
+  (location_id game_data /= Street) ==> not (won game_data)
